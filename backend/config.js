839c71df1345f12358a15e09871144de44da6b6a
Muhammad Tahir <119787687+Tahir326@users.noreply.github.com>
2024-06-22 21:16:17 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-    "mongodb+srv://sjaffri352:fkW3ELdlIgSt0Kcy@bookstoremern.ohnxgii.mongodb.net/?retryWrites=true&w=majority&appName=BookStoreMERN";
+   
 
